370e32e32841c2a1d74509b436f54a080ae3c702
vasyaqwe <elekton.fafik@gmail.com>
2023-01-17 12:32:10 +0200

    fixed back to homepage button

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion"
+import { useNavigate } from 'react-router-dom'
 
 function IconButton({ icon, className }) {
+    const navigate = useNavigate()
     const translate = {
         hover: {
             x: -6,
@@ -8,7 +10,7 @@ function IconButton({ icon, className }) {
     }
     return (
         <motion.button className={className} whileTap={{ scale: 0.95, translateY: '2px' }} >
-            <motion.span whileHover="hover" variants={translate}>{icon}</motion.span>
+            <motion.span onClick={() => navigate('/')} whileHover="hover" variants={translate}>{icon}</motion.span>
         </motion.button>
     )
 }
--- src/components/SecondaryHeader.jsx
+++ src/components/SecondaryHeader.jsx
@@ -5,16 +5,15 @@ import { Link } from 'react-router-dom'
 
 
 function SecondaryHeader() {
-
     return (
         <div className="secondary-header-wrapper | container">
             <header className="header secondary-header">
                 <img src={logo} alt="Indie Brew Logo" className="logo" />
                 <div className='flex'>
                     <IconButton className={'btn-icon'} text={'Back to homepage'} icon={<BackIcon />} />
-                    <Link to="/" className='hover-underline fs-600 fw-500 flex'>Back to homepage</Link>
+                    <Link to="/" className='hover-underline fs-600 fw-500 flex' > Back to homepage</Link>
                 </div>
-            </header>
+            </header >
         </div>
     )
 }
